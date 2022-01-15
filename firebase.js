4072f80191077f8a0374ace849110e40609839e6
scorpiyogi <sohani.codes@gmail.com>
2022-01-15 10:52:51 +0530

    solved firebase err

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app"
-import { getFirestore } from "firebase/firestore/lite"
+import { getFirestore } from "firebase/firestore"
 import { getAuth } from "firebase/auth"
 
 // TODO: Add SDKs for Firebase products that you want to use
--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,8 @@ import firebase from "firebase/app"
 import { auth, db } from "../firebase"
 import Loading from "../components/Loading"
 import { useEffect } from "react"
-import { updateDoc, serverTimestamp } from "firebase/firestore";
-import { collection, doc, setDoc} from "firebase/firestore";
+import { updateDoc, serverTimestamp } from "firebase/firestore"
+import * as firestore from "firebase/firestore"
 //import * as from "firebase/firestore"
 
 //import { updateDoc, serverTimestamp } from "firebase/firestore";
@@ -15,21 +15,20 @@ import { collection, doc, setDoc} from "firebase/firestore";
 
 // Update the timestamp field with the value from the server
 //const updateTimestamp = await updateDoc(docRef, {
-    //timestamp: serverTimestamp()
+//timestamp: serverTimestamp()
 //});
 
 function MyApp({ Component, pageProps }) {
   const [user, loading] = useAuthState(auth)
 
-  useEffect(() => {
+  useEffect(async () => {
     if (user) {
-      collection(db, "users")
-      setDoc(
-
-        doc(user.uid),
+      const usersRef = firestore.collection(db, "users")
+      await firestore.setDoc(
+        firestore.doc(usersRef, user.uid),
         {
           email: user.email,
-          lastSeen: firebase.firestore.FieldValue.serverTimestamp(),
+          lastSeen: firestore.serverTimestamp(),
           photoURL: user.photoURL
         },
         { merge: true }
